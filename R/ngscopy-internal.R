542adaadb0898f9c19e6d0a983446cabe2d979fb
Dan Tenenbaum <dtenenba@fhcrc.org>
2015-10-06 19:28:14 +0000

    fix various issues:   add zzz.R to collate field   replace == with >= in version specifiers in DESCRIPTION   remove from NAMESPACE functions that are not exported from changepoint   invoke those functions with ::: for now--long term an exported   alternative should be used.
    After these changes the vignette still does not build. Notifying maintainer.
    Package is still deprecated.
    
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/NGScopy@109257 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/ngscopy-internal.R
+++ R/ngscopy-internal.R
@@ -170,10 +170,10 @@
   segtype <- .parse_segmtype(segtype)
   segfunc <- switch(
     segtype,
-    mean.norm=func(changepoint::multiple.mean.norm,'multiple.mean.norm','changepoint'),
-    meanvar.norm=func(changepoint::multiple.meanvar.norm,'multiple.meanvar.norm','changepoint'),
-    mean.cusum=func(changepoint::multiple.mean.cusum,'multiple.mean.cusum','changepoint'),
-    var.css=func(changepoint::multiple.var.css,'multiple.var.css','changepoint')
+    mean.norm=func(changepoint:::multiple.mean.norm,'multiple.mean.norm','changepoint'),
+    meanvar.norm=func(changepoint:::multiple.meanvar.norm,'multiple.meanvar.norm','changepoint'),
+    mean.cusum=func(changepoint:::multiple.mean.cusum,'multiple.mean.cusum','changepoint'),
+    var.css=func(changepoint:::multiple.var.css,'multiple.var.css','changepoint')
     )
   invisible(segfunc)
 }
