7c0a2c5ad377be502a2416a645a8141b8bf633e6
Xiaobei Zhao <xiaobei@binf.ku.dk>
2015-10-07 19:47:18 +0000

    1.3.4| take two
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/NGScopy@109318 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,6 +1,31 @@
-.onAttach <- function(libname, pkgname) {
-    msg <- sprintf(
-        "Package '%s' is deprecated and will be removed from Bioconductor 
-         version %s", pkgname, "3.3")
-    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+
+## ************************************************************************
+## 
+## 
+## 
+## (c) Xiaobei Zhao
+## 
+## Wed Oct 07 12:31:21 EDT 2015 -0400 (Week 40)
+## 
+## 
+## Reference: 
+## 
+## 
+## ************************************************************************
+
+.onAttach <- function(libname, pkgname)
+{
+  f <- read.dcf(file.path(libname, pkgname, "DESCRIPTION"),
+                c("Version", "Date"))
+  url <- sprintf('https://www.bioconductor.org/packages/devel/bioc/html/%s.html',pkgname)
+  packageStartupMessage(
+    'Successfully loaded ', pkgname ,' package version ',f[1,1],'\n',
+    'Created on ', f[1,2],'\n',
+    'Please refer to the package NEWS for details of changes.','\n',
+    'You may find the latest version of ', pkgname ,' at: ','\n',
+    url,'\n',
+    'To suppress this message: \n',
+    'suppressPackageStartupMessages(require(',
+    pkgname,'))\n'
+    )
 }
